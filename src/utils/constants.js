0c941d976fa5c78d95224d12df1f0931c5b1d4bb
RahatinCode <ahmedrahat9901@gmail.com>
2026-01-31 00:01:18 +0600

    starting the habit tracker

---
+++ src/utils/constants.js
@@ -0,0 +1,28 @@
+export const HABIT_TYPES = {
+  BUILD: 'build',
+  BREAK: 'break',
+}
+
+export const REWARD_DELAY_MS = 2000
+
+export const STREAK_MILESTONES = [3, 7, 14, 21, 30, 60, 90, 180, 365]
+
+export const SOCIAL_FEED_LIMIT = 10
+
+export const HEATMAP_WEEKS = 16
+
+export const INTENSITY_LEVELS = {
+  NONE: 0,
+  LOW: 1,
+  MEDIUM: 2,
+  HIGH: 3,
+  MAX: 4,
+}
+
+export const TIME_BLOCK_MINUTES = [15, 30, 45, 60, 90, 120]
+
+export const IDENTITY_TEMPLATES = [
+  'I am a person who',
+  'I am becoming someone who',
+  'I am the type of person who',
+]
